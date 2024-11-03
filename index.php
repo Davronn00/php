eb08bd1170ae8d9b7813656a4ea9602d654162d9
Davronn00 <tugairoq04@gmail.com>
2024-11-03 09:33:38 +0100

    commit

--- index.php
+++ index.php
@@ -1,10 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method = "post" >
+    <label> Enter your username:</label> <br>
+    <input type = "text" name = "username" value = "try"><br>
+    <label> Enter your password:</label><br>
+    <input type = "text" name = "Password"><br>
+    <input type = "submit" name="Login" value = "Log in"><br>
+
+    </form>
+</body>
+</html>
 <?php
-/*$guys = array("Sherali", "Yuto", "Yhair", "Randita", "Davron");
-array_push($guys, "Suratjon", "Loic");
-array_shift($guys);
-array_pop($guys);
-$reversed_guys = array_reverse($guys);
-echo count($guys);
-foreach($guys as $guy){
-    echo $guy."<br>";}
-?>*/
+if($_POST["username"]){
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if(empty($username)){
+        echo "enter the username";
+    }
+    elseif(isset($username)){
+        echo "hello {$username}";
+    }
+    elseif(empty($password)){
+        echo "password should not be empty";
+ 
+    }
+    else{
+        echo "Welcome";
+    }
+    
+    
+}
+
+?>
