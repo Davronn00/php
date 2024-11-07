30ac6a497f7bf95e0c7ebdc4066662a4d0a09179
Davronn00 <tugairoq04@gmail.com>
2024-11-07 23:47:49 +0100

    checkboxes

--- index.php
+++ index.php
@@ -6,37 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    <form action = "index.php" method = "post">
-        <input type = "radio" name="credit_card" value = "Visa"> Visa <br>
-        <input type = "radio" name="credit_card " value = "Master card"> Master card <br>
-        <input type = "radio" name = "credit_card" value = "American express"> American express <br>
-        <input type = "submit" name = "confirm" value ="confirm"> <br><br>
+    <form action="index.php" method = "post" >
+    <input type = "checkbox" name = "foods[]" value = "Pizza"> 
+    Pizza <br>
+    <input type = "checkbox" name = "foods[]" value = "Hotdog">
+     Hotdog <br>
+    <input type = "checkbox" name = "foods[]" value = "Hamburger">
+     Hamburger <br>
+    <input type = "checkbox" name = "foods[]" value = "Osh">
+     Osh <br>
+    <input type = "submit" name = "submit"><br><br>
     </form>
 </body>
 </html>
 <?php
-    if(isset($_POST["confirm"])){
+    if(isset($_POST["submit"])){
 
-        $credit_card = null;
-        
-        if(isset($_POST["credit_card"])){
+        $foods = $_POST["foods"];
 
-            $credit_card = $_POST["credit_card"];
-        }
-        switch($credit_card){
-            case "Visa";
-                echo "You choose the Visa";
-                break;
-            case "Master card";
-                echo "You choose the Master card";
-                break;
-            case "American express";
-                echo "You choose the American express";
-                break;
-            default:
-                echo "You didn't choose the card";
-                break;
-        }
-
-        
+        foreach($foods as $food){
+        echo $food . "<br>";
     }
+    }
+?>
