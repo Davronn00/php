f990a8342435b49203370001c65f5ef0c16db113
Davronn00 <tugairoq04@gmail.com>
2024-11-08 00:09:39 +0100

    function

--- index.php
+++ index.php
@@ -1,31 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method = "post" >
-    <input type = "checkbox" name = "foods[]" value = "Pizza"> 
-    Pizza <br>
-    <input type = "checkbox" name = "foods[]" value = "Hotdog">
-     Hotdog <br>
-    <input type = "checkbox" name = "foods[]" value = "Hamburger">
-     Hamburger <br>
-    <input type = "checkbox" name = "foods[]" value = "Osh">
-     Osh <br>
-    <input type = "submit" name = "submit"><br><br>
-    </form>
-</body>
-</html>
 <?php
-    if(isset($_POST["submit"])){
-
-        $foods = $_POST["foods"];
-
-        foreach($foods as $food){
-        echo $food . "<br>";
+    function yotoqxona($ism, $davlat, $soha){
+        echo "{$ism} {$soha}da o'qiydi va u {$davlat}dan <br>";
     }
-    }
+    echo yotoqxona("Sherali", "Qirgiziston", "business administrator");
+    echo yotoqxona("Loic", "Madagaskar", "business administrator");
+    echo yotoqxona("Yuto", "Yaponiya", "engineering");
+    echo yotoqxona("Suratjon", "O'zbekiston", "PHD");
+    echo yotoqxona("Heshikbatrakh", "Mog'uliston", "computer science");
+
+
+
 ?>
