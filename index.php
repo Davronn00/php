38d0e0a70c6693042e4d970e7200d110827a1331
Davronn00 <tugairoq04@gmail.com>
2024-11-08 14:23:07 +0100

    function

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "index.php" method = "post">
+        <label>Aylana radiusini kiriting:</label><br>
+    <input type = "number" name = "Number"><br>
+    <input type = "submit" value = "login"><br>
+    </form>
+</body>
+
+</html>
 <?php
-    function yotoqxona($ism, $davlat, $soha){
-        echo "{$ism} {$soha}da o'qiydi va u {$davlat}dan <br>";
+    function hello($number): float{
+        $yuza = $number*pi()*2;
+        return $yuza;
     }
-    echo yotoqxona("Sherali", "Qirgiziston", "business administrator");
-    echo yotoqxona("Loic", "Madagaskar", "business administrator");
-    echo yotoqxona("Yuto", "Yaponiya", "engineering");
-    echo yotoqxona("Suratjon", "O'zbekiston", "PHD");
-    echo yotoqxona("Heshikbatrakh", "Mog'uliston", "computer science");
-
-
-
+    
+        $yuzaa = null;    
+        $radius = $_POST["Number"];
+        $yuzaa = hello($radius);
+        echo "Aylana yuzasi {$yuzaa}ga teng";
+    
 ?>
