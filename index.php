608d53459d1bc381683f0e759e5b9633357515cb
Davronn00 <tugairoq04@gmail.com>
2024-11-10 15:53:16 +0100

    cookie

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
-<?php
-   include("header.html")
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This head page of the website
-</body>
-</html>
-<?php
-    include("footer.html")
-?>
+<?php 
+    setcookie("fav_food", "osh", time()-0, "/");
+    setcookie("fav_drink", "choy", time()+ (86400*1), "/");
+    setcookie("fav_desert", "non", time()+ (86400*62), "/");
+
+    /*f(isset($_COOKIE["fav_food"])){
+        echo "Your favourite food is {$_COOKIE["fav_food"]}";
+    }
+    else{
+        echo "I don't know your favourite food";
+    }*/
+foreach($_COOKIE as $key => $value){
+    echo "$key is $value <br>";
+}
+if(isset($_COOKIE["fav_food"])){
+    echo "Your favourite food is {$_COOKIE["fav_food"]}";
+}
+else{
+    echo "I don't know your favourite food <br>";
+}
+if(isset($_COOKIE["fav_drink"])){
+    echo "You want to drink {$_COOKIE["fav_drink"]}<br>";
+}
+else{
+    echo "I don't know your favourite food";
+}
+
+?> 
