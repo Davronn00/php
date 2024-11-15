7042edaa5d7061eb32a87bc5c9839783ae60e10c
Davronn00 <tugairoq04@gmail.com>
2024-11-15 19:29:15 +0100

    login2

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-include("database.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,24 +19,35 @@ include("database.php");
 </body>
 </html>
 <?php
-   
+    
 
     if(isset($_POST["login"])){
+        include("database.php");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO users (user, password)
+            VALUES ($username,$hash)";
         if(!empty($_POST["username"] && 
             !empty($_POST["password"]))){
 
-        $_SESSION["username"]=$_POST["username"];
-        $_SESSION["password"]=$_POST["password"];    
+        $_SESSION["username"]= $username;
+        $_SESSION["password"]= $password;
 
+        try{
+            mysqli_query($conn, $sql);
+            echo"user is now registered";
+        }
+        catch(mysqli_sql_exception){
+            echo "couldn't register user";
+        }
         header("Location: game.php"); 
         }
         else{
             echo "Username or password shouldn't be empty";
         
         }
-                
-           
-        
     }
 
 ?>
